568c7d54461e8ed25620e4d045eca547e03f2e8b
onevaz <60717088+onevaz@users.noreply.github.com>
2020-02-10 16:32:00 -0600

    convierte calificacion a usa

---
+++ calificacion.js
@@ -0,0 +1,14 @@
+var num1=Math.floor(Math.random()*10)
+console.log(num1);
+if(num1==10)
+ console.log("tu calificacion es una A");
+else if(num1==9)
+ console.log("tu calificacion es una B");
+else if(num1==8)
+ console.log("tu calificacion es una C");
+else if(num1==7)
+ console.log("tu calificacion es una D");
+else if(num1==6)
+ console.log("tu calificacion es una E");
+else if(num1<=5)
+ console.log("tu calificacion es una F");
